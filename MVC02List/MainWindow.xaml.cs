11c994da55ad5659a2bbd07fbf72c1a76f10fb40
Artem <a.khimin@yandex.ru>
2022-03-24 23:58:01 +0300

    Сделал 1 задание

---
+++ MVC02List/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using MVC02List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MVC02List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        Wrapper wrapper = new Wrapper();
+
+
+        static int Fiban(int n)
+        {
+            if (n == 1 || n == 2) return 1;
+            else return Fiban(n - 1) + Fiban(n - 2);
+        }
+
+        private void bAdd_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() =>
+            {
+                int i = wrapper.list.Count+1;
+
+                 tbText.Dispatcher.BeginInvoke(
+                async () =>
+                {
+                    int fb = Fiban(i);
+
+                    string li = $"Число №{i} = {fb}" + Environment.NewLine;
+                    wrapper.list.Add(li);
+
+                    tbText.Text += wrapper.list[i-1];
+                });
+            }).Start();
+        }
+
+        private void bDelete_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() =>
+            {
+                
+
+                tbText.Dispatcher.BeginInvoke(
+                () =>
+                {
+                    if (wrapper.list.Count>0)
+                    {
+                        wrapper.list.RemoveAt(wrapper.list.Count - 1);
+
+                        tbText.Text = string.Empty;
+
+                        for (int i = 0; i < wrapper.list.Count; i++)
+                        {
+                            tbText.Text += wrapper.list[i];
+                        }
+                    }
+             
+                });
+            }).Start();
+        }
+    }
+}
