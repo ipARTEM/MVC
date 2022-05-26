7779871369b0ca8c83c98220665f3765ccb89a41
Artem Khimin <artem.khimin@digital-jurist.ru>
2022-05-26 20:59:05 +0300

    Добавил проект  MVC10Project

--- MVC09Project/DoNotHaveWords.cs
+++ MVC09Project/DoNotHaveWords.cs
@@ -9,21 +9,21 @@ namespace MVC09Project
         {
             _words = words;
         }
-        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
-        {
-            var bad = new HashSet<string>(_words, StringComparer.InvariantCultureIgnoreCase);
-            var request = (CreateWeatherForecastRequest)validationContext.ObjectInstance;
-            if (string.IsNullOrEmpty(request.Summary)) return
-            ValidationResult.Success;
-            var words = new HashSet<string>(request.Summary.Split(" "),
-            StringComparer.InvariantCultureIgnoreCase);
-            foreach (var word in words)
-            {
-                if (bad.Contains(word))
-                    return new ValidationResult("Описание содержит запрещенные слова", new[] { nameof(request.Summary) });
-            }
-            return ValidationResult.Success;
-        }
+        //protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        //{
+        //    //var bad = new HashSet<string>(_words, StringComparer.InvariantCultureIgnoreCase);
+        //    //var request = (CreateWeatherForecastRequest)validationContext.ObjectInstance;
+        //    //if (string.IsNullOrEmpty(request.Summary)) return
+        //    //ValidationResult.Success;
+        //    //var words = new HashSet<string>(request.Summary.Split(" "),
+        //    //StringComparer.InvariantCultureIgnoreCase);
+        //    //foreach (var word in words)
+        //    //{
+        //    //    if (bad.Contains(word))
+        //    //        return new ValidationResult("Описание содержит запрещенные слова", new[] { nameof(request.Summary) });
+        //    //}
+        //    //return ValidationResult.Success;
+        //}
 
     }
 }
