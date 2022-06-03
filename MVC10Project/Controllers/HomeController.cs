2d6dc79c28691e87745dd18b05973ae9db5657e4
Artem Khimin <artem.khimin@digital-jurist.ru>
2022-06-03 21:11:07 +0300

    Сделал 8е задание

--- MVC10Project/Controllers/HomeController.cs
+++ MVC10Project/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MVC10Project.MailKits;
 using MVC10Project.Models;
 using System.Diagnostics;
 
@@ -17,6 +20,50 @@ namespace MVC10Project.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        public async Task<IActionResult> SendMail(string name, string email, string msg, string header)
+        {
+            var message = new MimeMessage();
+
+            string mailBoxSender = "FROMtest@yandex.ru";
+
+            message.From.Add(new MailboxAddress(name, mailBoxSender));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = header;
+
+            message.Body = new TextPart("html")
+            {
+                Text = "Сообщение от: " + name + " <br>" +
+                "С почтового ящика: " + mailBoxSender + "<br>" +
+                "Текст сообщения: " + msg
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 465);
+                await client.AuthenticateAsync(mailBoxSender, "*************");
+                await client.SendAsync(message);
+
+                await client.DisconnectAsync(true);
+            }
+            return View("Contact");
+        }
+
+        public async Task<IActionResult> SendMessage()
+        {
+            EmailService emailService = new EmailService();
+            await emailService.SendEmailAsync("TOtext@yandex.ru", "Новое письмо из сервиса", "Текст письма");
+
+
+            return View("SendMessage");
+        }
+
 
         public IActionResult Privacy()
         {
---
+++ MVC10Project/MailKits/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace MVC10Project.MailKits
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Админ", "FROMtest@yandex.ru"));    
+            emailMessage.To.Add(new MailboxAddress("", email));         
+            emailMessage.Subject = subject;
+           
+
+            emailMessage.Body = new TextPart("Plain")   
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 465);        
+                await client.AuthenticateAsync("FROMtest@yandex.ru", "3Gb-SJW-Tdf-293");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
